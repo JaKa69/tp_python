7f4c7c7e89afc50800b91c3ae3047508e67995f8
alix babin <alix.babin69@gmail.com>
2024-03-14 12:31:54 +0100

    début connexion bdd

--- index.py
+++ index.py
@@ -1,4 +1,6 @@
 import sqlite3
+from pathlib import Path
+
 from flask import Flask, render_template, g
 
 app = Flask(__name__)
@@ -12,6 +14,12 @@ def get_db():
     db.row_factory = sqlite3.Row
     return db
 
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
@@ -21,7 +29,17 @@ def close_connection(exception):
 @app.route("/login")
 def login():
     return render_template('login.html')
+
 @app.route("/")
 def create_app():
-    cur = get_db().cursor()
+    for user in query_db('select * from user'):
+        print(user['username'], 'has the id', user['id'])
     return render_template("home.html")
+
+
+if not Path(DATABASE).exists():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('db/schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
